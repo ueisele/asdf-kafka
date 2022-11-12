ad67db148d063656987cc1f07f9a7bdcd0b95770
Uwe Eisele <code@uweeisele.eu>
2022-11-12 14:37:14 +0100

    refactor: utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -16,22 +16,36 @@ curl_opts=(-fsSL)
 
 list_confluent_versions() {
 	# extract available major versions from html page
-	local versions="$(curl $curl_opts "${CONFLUENT_PACKAGES_URL}/" | sed -rn 's/^[ ]*<a href="\/archive\/.*\/">(.*)\/<\/a>/\1/p' | tr '\n' ' ')"
+	local versions="$(
+		curl $curl_opts "${CONFLUENT_PACKAGES_URL}/" |
+			sed -rn 's/^[ ]*<a href="\/archive\/.*\/">(.*)\/<\/a>/\1/p' | tr '\n' ' '
+	)"
 	# enterprise
 	for version in $versions; do
 		# extract available versions from html page (without scala version)
-		curl $curl_opts ${CONFLUENT_PACKAGES_URL}/${version}/ | sed -rn 's/^.*<a.*>confluent-([0-9\.]*)(-[0-9\.]*)?\.tar\.gz<\/a>/confluent-enterprise-\1/p' | sort -V | uniq
+		# output pattern: confluent-enterprise-<version>
+		curl $curl_opts ${CONFLUENT_PACKAGES_URL}/${version}/ |
+			sed -rn 's/^.*<a.*>confluent-([0-9\.]*)(-[0-9\.]*)?\.tar\.gz<\/a>/confluent-enterprise-\1/p' |
+			sort -V |
+			uniq
 	done
 	# community
 	for version in $versions; do
 		# extract available versions from html page (without scala version)
-		curl $curl_opts ${CONFLUENT_PACKAGES_URL}/${version}/ | sed -rn 's/^.*<a.*>confluent-(community|oss)-([0-9\.]*)(-[0-9\.]*)?\.tar\.gz<\/a>/confluent-community-\2/p' | sort -V | uniq
+		# output pattern: confluent-community-<version>
+		curl $curl_opts ${CONFLUENT_PACKAGES_URL}/${version}/ |
+			sed -rn 's/^.*<a.*>confluent-(community|oss)-([0-9\.]*)(-[0-9\.]*)?\.tar\.gz<\/a>/confluent-community-\2/p' |
+			sort -V |
+			uniq
 	done
 }
 
 list_apache_versions() {
 	# extract available versions from html page (without scala version)
-	curl $curl_opts "${APACHE_PACKAGES_URL}/" | sed -rn 's/^.*<a href=.*>([0-9\.]*)\/<\/a>.*$/apache-\1/p' | sort -V
+	# output pattern: apache-<version>
+	curl $curl_opts "${APACHE_PACKAGES_URL}/" |
+		sed -rn 's/^.*<a href=.*>([0-9\.]*)\/<\/a>.*$/apache-\1/p' |
+		sort -V
 }
 
 list_all_versions() {
@@ -44,16 +58,24 @@ confluent_download_url() {
 	version="$1"
 	# extract first two version numbers
 	majorversion="$(echo ${version} | sed -rn 's/^.*-([0-9]*\.[0-9]*)(\.[0-9]*)*$/\1/p')"
-	# resolve actual version name for download
+	# resolve actual version name for download, depending on release type
 	if [[ ${version} =~ ^confluent-enterprise ]]; then
+		# confluent-enterprise-<version> -> confluent-<version>
 		downloadversion="$(echo ${version} | sed -rn 's/^.*-([0-9\.]*)$/confluent-\1/p')"
 	elif [[ ${version} =~ ^confluent-community-[34] ]] || [[ ${version} =~ ^confluent-community-5\.0 ]]; then
+		# confluent-community-<version =~ [34]|5.0> -> confluent-oss-<version>
 		downloadversion="$(echo ${version} | sed -rn 's/^.*-([0-9\.]*)$/confluent-oss-\1/p')"
 	else
 		downloadversion="${version}"
 	fi
-	# extract most current scala version if present (only until 5.0)
-	scalapostfix="$(curl $curl_opts ${CONFLUENT_PACKAGES_URL}/${majorversion}/ | sed -rn "s/^.*${downloadversion}(-[0-9\.]*)?\.tar\.gz<\/a>/\1/p" | sort | tail -1)"
+	# determine most current scala version if present (only until 5.0)
+	scalapostfix="$(
+		curl $curl_opts ${CONFLUENT_PACKAGES_URL}/${majorversion}/ |
+			sed -rn "s/^.*${downloadversion}(-[0-9\.]*)?\.tar\.gz<\/a>/\1/p" |
+			sort |
+			tail -1
+	)"
+	# output download url
 	echo "${CONFLUENT_PACKAGES_URL}/${majorversion}/${downloadversion}${scalapostfix}.tar.gz"
 }
 
@@ -63,13 +85,20 @@ apache_download_url() {
 	# remove 'apache-' from version
 	downloadversion="$(echo ${version} | sed -rn 's/^apache-(.*)$/\1/p')"
 	# extract most current scala version
-	scalaversion="$(curl $curl_opts "${APACHE_PACKAGES_URL}/${downloadversion}/" | sed -rn 's/^.*<a href=.*>kafka_([0-9\.]*)-.*$/\1/p' | uniq | sort -V | tail -1)"
+	scalaversion="$(
+		curl $curl_opts "${APACHE_PACKAGES_URL}/${downloadversion}/" |
+			sed -rn 's/^.*<a href=.*>kafka_([0-9\.]*)-.*$/\1/p' |
+			uniq |
+			sort -V |
+			tail -1
+	)"
 	# determine extension (0.8.0 has .tar.gz all others have .tgz)
 	if [[ "${downloadversion}" == "0.8.0" ]]; then
 		extension="tar.gz"
 	else
 		extension="tgz"
 	fi
+	# output download url
 	echo "${APACHE_PACKAGES_URL}/${downloadversion}/kafka_${scalaversion}-${downloadversion}.${extension}"
 }
 
@@ -77,7 +106,7 @@ download_release() {
 	local version filename url
 	version="$1"
 	filename="$2"
-
+	# determine url depending on release type
 	if [[ ${version} =~ ^confluent- ]]; then
 		url="$(confluent_download_url "${version}")"
 	elif [[ ${version} =~ ^apache- ]]; then
@@ -85,7 +114,7 @@ download_release() {
 	else
 		fail "asdf-$TOOL_NAME does not support downloading release ${version}."
 	fi
-
+	# download the release
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
